424982d1df34b35313d90b1d5dd1cec305dd81ae
Dominic Meiser <dmeiser79@gmail.com>
2016-11-26 17:19:16 -0700

    Add skeleton for gtest and cmake configuration.

---
+++ include/image.h
@@ -0,0 +1,26 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <memory>
+
+
+namespace Mo {
+
+class Image {
+  Image(const char* filename);
+  void save(const char* filename);
+  char *getPixelData();
+  const char *getConstPixelData() const;
+  int width() const;
+  int height() const;
+  int pitch() const;
+
+private:
+  class Impl;
+  std::unique_ptr<Impl> impl_;
+};
+
+}
+
+#endif
+
