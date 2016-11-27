d666be39d25319b8bdcc6adbd6dbcad7fdf0f8d4
Dominic Meiser <dmeiser79@gmail.com>
2016-11-27 12:42:03 -0700

    Finish implementation of command line options.

--- src/run_configuration.cpp
+++ src/run_configuration.cpp
@@ -8,6 +8,7 @@ RunConfiguration::RunConfiguration(int n, const char *argv[]) {
   if (n < 1) {
     throw std::runtime_error("Invalid command line arguments.");
   }
+  std::vector<int> used(n, 0);
 
   int i;
   for (i = 1; i != n; ++i) {
@@ -16,8 +17,8 @@ RunConfiguration::RunConfiguration(int n, const char *argv[]) {
         throw std::runtime_error("Invalid target image.");
       }
       targetImageName_ = argv[i + 1];
-      argv[i] = 0;
-      argv[i + 1] = 0;
+      used[i] = 1;
+      used[i + 1] = 1;
       break;
     }
   }
@@ -27,13 +28,39 @@ RunConfiguration::RunConfiguration(int n, const char *argv[]) {
 
   // Done parsing options, now obtain the tile file names.
   for (i = 1; i != n; ++i) {
-    if (argv[i]) {
+    if (!used[i]) {
       tiles_.push_back(argv[i]);
+      used[i] = 1;
     }
   }
   if (tiles_.empty()) {
     throw std::runtime_error("Must provide at least one tile.");
   }
+
+  // Generated output file name if none provided.
+  if (outputImageName_.empty()) {
+    outputImageName_ = std::string("out_") + targetImageName_;
+  }
+}
+
+std::string* RunConfiguration::tileBegin() {
+  // The only way to construct a RunConfiguration is
+  // with more than one tile.  Hence this is legal.
+  return &tiles_[0];
+}
+
+std::string* RunConfiguration::tileEnd() {
+  // The only way to construct a RunConfiguration is
+  // with more than one tile.  Hence this is legal.
+  return &tiles_[tiles_.size() - 1] + 1;
+}
+
+std::string* RunConfiguration::targetImageName() {
+  return &targetImageName_;
+}
+
+std::string* RunConfiguration::outputImageName() {
+  return &outputImageName_;
 }
 
 }
--- tests/run_configuration_test.cpp
+++ tests/run_configuration_test.cpp
@@ -20,3 +20,24 @@ TEST(RunConfiguration, MustProvideAtLeastOneTile) {
   EXPECT_THROW(Mo::RunConfiguration(3, argv), std::runtime_error);
 }
 
+TEST(RunConfiguration, HeresALegalConfiguration) {
+  const char *argv[] = {"blah",
+      "-t", "targetImage.jpg",
+      "aTile.jpg",
+      "anotherTile.jpg"};
+  Mo::RunConfiguration config(5, argv);
+  EXPECT_EQ(2, std::distance(config.tileBegin(), config.tileEnd()));
+  ASSERT_NE(nullptr, config.targetImageName());
+  EXPECT_EQ(std::string(argv[2]), *config.targetImageName());
+}
+
+TEST(RunConfiguration, OutputNameIsGeneratedIfNotSupplied) {
+  const char *argv[] = {"blah",
+      "-t", "targetImage.jpg",
+      "aTile.jpg",
+      "anotherTile.jpg"};
+  Mo::RunConfiguration config(5, argv);
+  ASSERT_NE(nullptr, config.outputImageName());
+  EXPECT_FALSE(config.outputImageName()->empty());
+}
+
