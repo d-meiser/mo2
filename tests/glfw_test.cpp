ef7dff95afc58f97a83e463be7b4e8e1c314d82f
Dominic Meiser <dmeiser79@gmail.com>
2016-12-03 11:11:58 -0700

    Add glfw_test.cpp.

---
+++ tests/glfw_test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <GLFW/glfw3.h>
+
+
+TEST(glfw, CanCreateOffscreenContext) {
+  GLFWwindow* window;
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_VISIBLE, false);
+  window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+  EXPECT_NE(nullptr, window);
+  glfwDestroyWindow(window);
+}
+
+int main(int argn, char* argv[]) {
+  ::testing::InitGoogleTest(&argn, argv);
+  glfwInit();
+  int result = RUN_ALL_TESTS();
+  glfwTerminate();
+  return result;
+}
