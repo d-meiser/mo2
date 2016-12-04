ff68fdb6fd2d2957c52cdf990d5eeabe0b11535a
Dominic Meiser <dmeiser79@gmail.com>
2016-12-03 17:21:49 -0700

    Add gl3w.

--- tests/glfw_test.cpp
+++ tests/glfw_test.cpp
@@ -2,14 +2,25 @@
 #include <GLFW/glfw3.h>
 
 
-TEST(glfw, CanCreateOffscreenContext) {
+static GLFWwindow* createWindow() {
   GLFWwindow* window;
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_VISIBLE, false);
   window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+  return window;
+}
+
+TEST(glfw, CanCreateOffscreenContext) {
+  GLFWwindow* window = createWindow();
   EXPECT_NE(nullptr, window);
+  glfwDestroyWindow(window);
+}
+
+TEST(glfw, CanCreateFBO) {
+  GLFWwindow* window = createWindow();
+  ASSERT_NE(nullptr, window);
   glfwDestroyWindow(window);
 }
 
